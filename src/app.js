a83a43e31b03f0d65f0975ea63ff5508dbf04e38
beycker <beycker_111@hotmail.com>
2020-12-08 15:58:29 -0500

    Se agrego el front

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', indexRoutes);
+//app.use('/', indexRoutes);
 app.use('/users', usersRoutes);
 app.use('/zones', zoneRoutes);
 
